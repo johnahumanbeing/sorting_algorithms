6321dd6aff8a9d7702571f7489ab0d43652af3e2
programmer-88 <ianmbayyz@gmail.com>
2023-11-17 01:06:36 +0300

    bubble sort fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,23 +2,19 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int swapped;
-    long unsigned int i, j;
+    unsigned long i, j;
 
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) 
+    print_array(array, size);
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
         {
-            if (array[j] > array[j + 1]) 
+            if (array[j] > array[j + 1])
             {
-                swap(&array[j], &array[j + 1]);   
-                swapped = 1;   
+                swap(&array[j], &array[j + 1]);
+                print_array(array, size);
             }
-        }
-        print_array(array, size);
-
-        if (swapped == 0)
-        {
-            break;
         }
     }
 }
