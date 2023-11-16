ce809e3a1ac422539493a836dafbba6e7bbd5a39
johnahumanbeing <mseewak@gmail.com>
2023-11-17 00:54:02 +0300

    whips

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,9 @@ void bubble_sort(int *array, size_t size)
         for (j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j + 1]) 
                 {
-                swap(&array[j], &array[j + 1]);       
+                    int temp = array[j];      
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
         }
     }
