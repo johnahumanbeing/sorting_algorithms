39b8df5f0aba29ca0984dc51f1d3dc72a4105faf
johnahumanbeing <mseewak@gmail.com>
2023-11-17 01:01:44 +0300

    function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,14 +8,7 @@ void bubble_sort(int *array, size_t size)
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) 
         {
-            if (array[j] > array[j + 1]) 
-<<<<<<< HEAD
-                {
-                    int temp = array[j];      
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-=======
+            if (array[j] > array[j + 1])
             {
                 swap(&array[j], &array[j + 1]);   
                 swapped = 1;   
@@ -26,7 +19,6 @@ void bubble_sort(int *array, size_t size)
         if (swapped == 0)
         {
             break;
->>>>>>> 7f1fc718dcaf4968b39e8e3214cf48b2a6419c71
         }
     }
 }
