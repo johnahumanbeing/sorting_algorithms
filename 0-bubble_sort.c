aa782255df0835d8ad10b3f2b819453d65da1459
programmer-88 <ianmbayyz@gmail.com>
2023-11-17 23:48:55 +0300

    fixed bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,11 @@ void bubble_sort(int *array, size_t size)
 
 	print_array(array, size);
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
