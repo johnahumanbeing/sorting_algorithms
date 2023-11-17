20936c87bb2ef976d022fd19b798d0136f42b643
johnahumanbeing <mseewak@gmail.com>
2023-11-17 23:50:09 +0300

    remove the printf

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,10 +8,8 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned long i, j;
+	size_t i, j, k;
 	int temp;
-
-	print_array(array, size);
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -19,11 +17,17 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
 
-				print_array(array, size);
+				temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				for (k = 0; k < size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
 			}
 		}
 	}
