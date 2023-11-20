e8fdc5fb6302f89a4bebc6ee952d710c118a3d6b
johnahumanbeing <mseewak@gmail.com>
2023-11-20 20:28:27 +0300

    quick sortfix 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,42 +9,27 @@
 
 size_t lomutopartition(int *array, int low, int high)
 {
-	int pivot = array[high];
+	int pivot = array[low];
 	int temp;
-	size_t i = low - 1, j;
+	size_t i = low;
 
-	for (j = low; j <= (size_t)(high - 1); j++)
+	for (size_t j = low + 1; <= (size_t)high; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
 			temp = array[i];
+
 			array[i] = array[j];
 			array[j] = temp;
 		}
 	}
 
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
+	temp = array[i];
+	array[i] = array[low];
+	array[j] = temp;
 
-	return (i + 1);
-}
-
-/**
- * quick_sort - sort the array using
- * quick sort algorithm
- * @array: array to sort
- * @size: sizeof array
- * Return: is void
-*/
-
-void quick_sort(int *array, size_t size)
-{
-	if (array == NULL || size < 2)
-		return;
-
-	quick_sort_alg(array, 0, size - 1);
+	return (i);
 }
 
 /**
@@ -63,4 +48,19 @@ void quick_sort_alg(int *array, int low, int high)
 		quick_sort_alg(array, low, pivot - 1);
 		quick_sort_alg(array, pivot + 1, high);
 	}
+}
+
+/**
+ * quick_sort - sort the array using
+ * quick sort algorithm
+ * @array: array to sort
+ * @size: sizeof array
+ * Return: is void
+*/
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_alg(array, 0, size - 1);
 }
