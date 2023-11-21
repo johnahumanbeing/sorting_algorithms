4d1ea2b178c942f4999d7cb88005f6b84ace14b1
programmer-88 <ianmbayyz@gmail.com>
2023-11-22 01:17:49 +0300

    quick sort fixed

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,81 +1,56 @@
 #include "sort.h"
-/**
- * lomutopartition - gets the size of array
- * @array: array to check
- *@lomuto: the first index (highest)
- * @end: lowest in array (end index)
- * @size: the size of the array
- * Return: size of array
-*/
-
-size_t lomutopartition(int *array, size_t lomuto, size_t end, size_t size)
-{
-	int pivot = array[end], temp;
-	size_t i = lomuto - 1, j;
-
-	for (j = lomuto; j < end; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-
-			if (i != j)
-			{
-				temp = array[j];
-				array[j] = array[i];
-				array[i] = temp;
-
-				print_array(array, size);
-			}
-		}
-	}
-
-	i++;
-
-	if (array[i] > pivot)
-	{
-		temp = array[i];
-		array[i] = array[end];
-		array[end] = temp;
-
-		print_array(array, size);
-
-	}
-
-	return (i);
-}
 
 /**
- * quick_sort_alg - sorts using divide and conqoer
- * @array: array to check
- * @low: lower end
- * @high: higher end
- *@size: the size of the array
- * Return: is void
-*/
-void quick_sort_alg(int *array, int low, int high, size_t size)
-{
-	size_t pivot;
-
-	if (low < high)
-	return;
-
-	pivot = lomutopartition(array, low, high, size);
-
-	quick_sort_alg(array, low, pivot - 1, size);
-	quick_sort_alg(array, pivot + 1, high, size);
-}
-
-/**
- * quick_sort - sort the array using uick sort algorithm
+ * quick_sort - calls the sorting algorithm
  * @array: array to sort
- * @size: size of array
+ * @size: sizeof array
  * Return: is void
 */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-			return;
+	quick_sort_alog(array, 0, size);
+}
 
-	quick_sort_alg(array, 0, size - 1, size);
+/**
+ * quick_sort_alog -function that actually sorts recursively
+ * @array: array to sort
+ * @size: sizeof array
+ * Return: is void
+*/
+void quick_sort_alog(int *array, int s, int end)
+{
+	if (end <= s)
+	{
+		return;
+	}
+	int pivot = partition(array, s, end);
+	quick_sort_alog(array, s, pivot - 1);
+	quick_sort_alog(array, pivot + 1, end);
+}
+
+/**
+ * partition - uses divide and conquer technique to sort
+ * @array: array to sort
+ * 
+*/
+int partition(int *array, int s, int end)
+{
+	int pivot = array[end];
+	int i = s - 1;
+
+	for (int j = s; j <= end; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			int temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
+	i++;
+	int temp = array[i];
+	array[i] = array[end];
+	array[end] = temp;
+	return (i);
 }
--- sort.h
+++ sort.h
@@ -23,8 +23,11 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+void quick_sort_alog(int *array, int s, int end);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+int partition(int *array, int s, int end);
+void quick_sort(int *array, size_t size);
 
 #endif
+
