30bf985d41f5f5ca63b30500f49b2c6218b0f4e8
johnahumanbeing <mseewak@gmail.com>
2023-11-21 21:50:28 +0300

    function that sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,85 +7,80 @@
 *@t: tail
 *Return: nothing to return
 */
-void swap_nodes(listint_t *na, listint_t *nb, listint_t **list,  listint_t **t) 
+void swap_nodes(listint_t *na, listint_t *nb, listint_t **list, listint_t **t)
 {
-    listint_t *temp, *curr, *next;
+	listint_t *temp, *curr, *next;
 
-    temp = na->temp;
-    curr = na;
-    next = nb;
+	temp = na->prev;
+	curr = na;
+	next = nb;
 
-    curr->next = next->next;
-    next->temp = temp;
+	curr->next = next->next;
+	next->prev = temp;
 
-    if (temp)
-            temp->next = next;
+	if (temp)
+		temp->next = next;
 
-    if (next->next)
-            next->next->temp = curr;
+	if (next->next)
+		next->next->prev = curr;
+	curr->prev = next;
+	next->next = curr;
 
-    curr->temp = next;
-    next->next = curr;
+	if (!(next->prev))
+		*list = next;
 
-    if (!(next->temp))
-            *list = next;
+	if (!(curr->next))
+		*t = curr;
 
-    if (!(curr->next))
-            *t = curr;
-
-    print_list(*list);
+	print_list(*list);
 }
+
 
 /**
-*cocktail_sort_list - sorts a doubly linked list of integers in ascending order using 
-*                   the Cocktail shaker sort algorithm
+*cocktail_sort_list - sorts a doubly linked list of integers in ascending
+*	order using the Cocktail shaker sort algorithm
 *@list: the doubly linked list to be sorted
 *Return: nothing to return
-*/    
-/*
-yet to fix this part
-void cocktail_sort_list(listint_t **list) 
-{
-    int swapped;
-    listint_t *ptr;
-    listint_t *head = *list;
-
-    if (head == NULL)
-        return;
-
-    do {
-        swapped = 0;
-        ptr = head;
-
-        while (ptr->next != NULL) 
-        {
-            if (ptr->n > ptr->next->n) 
-            {
-                swap_nodes(ptr, &(ptr->next), list);
-                swapped = 1;
-                print_list(*list);
-            }
-            else
-                ptr = ptr->next;
-        }
-
-        if (!swapped)
-            break;
-
-        swapped = 0;
-        ptr = ptr->prev;
-
-        while (ptr->prev != NULL) 
-        {
-            if (ptr->n < ptr->prev->n) 
-            {
-                swap_nodes(&(ptr->prev), &ptr, list);
-                swapped = 1;
-                print_list(*list);
-            }
-            else
-                ptr = ptr->prev;
-        }
-    } while (swapped);
-}
 */
+void cocktail_sort_list(listint_t **list)
+{
+	int swapped;
+	listint_t *ptr, *term;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	term = *list;
+	while (term->next)
+		term = term->next;
+
+	do {
+		swapped = 0;
+		ptr = *list;
+		while (ptr)
+		{
+			while (ptr->next && ptr->n > ptr->next->n)
+			{
+				swap_nodes(ptr, ptr->next, list, &term);
+				swapped = 1;
+				ptr = ptr->prev;
+			}
+			ptr = ptr->next;
+		}
+		if (!swapped)
+			break;
+
+		swapped = 0;
+		ptr = term;
+		while (ptr)
+		{
+			while (ptr->prev && ptr->n < ptr->prev->n)
+			{
+				swap_nodes(ptr->prev, ptr, list, &term);
+				swapped = 1;
+				ptr = ptr->next;
+			}
+			ptr = ptr->prev;
+		}
+	} while (swapped);
+}
