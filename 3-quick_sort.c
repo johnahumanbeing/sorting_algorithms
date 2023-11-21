40517fb50784cf8575f7b0fec2b640f14684a17c
programmer-88 <ianmbayyz@gmail.com>
2023-11-22 01:41:26 +0300

    quick sort fixed

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,21 +9,25 @@
 void quick_sort(int *array, size_t size)
 {
 	quick_sort_alog(array, 0, size);
+
 }
 
 /**
  * quick_sort_alog -function that actually sorts recursively
  * @array: array to sort
- * @size: sizeof array
+ * @s: start of array
+ * @end: end of array
  * Return: is void
 */
 void quick_sort_alog(int *array, int s, int end)
 {
+	int pivot;
+
 	if (end <= s)
 	{
 		return;
 	}
-	int pivot = partition(array, s, end);
+	pivot = partition(array, s, end);
 	quick_sort_alog(array, s, pivot - 1);
 	quick_sort_alog(array, pivot + 1, end);
 }
@@ -31,25 +35,28 @@ void quick_sort_alog(int *array, int s, int end)
 /**
  * partition - uses divide and conquer technique to sort
  * @array: array to sort
- * 
+ * @s: starting of array
+ * @end: end of array
+ * Return: sorted partition
+ *
 */
 int partition(int *array, int s, int end)
 {
 	int pivot = array[end];
-	int i = s - 1;
+	int temp, j, i = s - 1;
 
-	for (int j = s; j <= end; j++)
+	for (j = s; j <= end; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			int temp = array[i];
+			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
 		}
 	}
 	i++;
-	int temp = array[i];
+	temp = array[i];
 	array[i] = array[end];
 	array[end] = temp;
 	return (i);
